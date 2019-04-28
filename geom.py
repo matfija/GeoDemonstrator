0ca50acc74defa980ba1aac4ae731dfd54aa27d3
matfija <lazar1997@mts.rs>
2019-04-28 04:34:43 +0200

    GKI modul i test klasa

---
+++ geom.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+# Ukljucivanje matematickog modula
+from math import sin, cos, radians
+
+# Geometrijska transformacija ravni
+# predstavljena homogenom 3x3 matricom
+class geom:
+  # Konstruktor transformacije
+  def __init__(self, mat):
+    self.mat = mat
+  
+  # Uobicajeno mnozenje matrica ili, pak,
+  # mnozenje matrice sa tackom prostora
+  def __mul__(self, dr):
+    if isinstance(dr, geom):
+      pom = [[0 for i in range(3)] for j in range(3)]
+        
+      for i in range(3):
+        for j in range(3):
+          for k in range(3):
+            pom[i][j] += self.mat[i][k] * dr.mat[k][j]
+
+      return geom(pom)
+    else:
+      pom = [0 for i in range(3)] 
+      for i in range(3):
+        for j in range(3):
+          pom[i] += self.mat[i][j] * dr.mat[j]
+      
+      return tacka(pom)
+  
+  # Uobicajena string predstava matrice
+  def __str__(self):
+    return str(self.mat[0]) + '\n' \
+         + str(self.mat[1]) + '\n' \
+         + str(self.mat[2])
+
+# Translacija nasledjuje geom. trans.
+class trans(geom):
+  def __init__(self, x=0, y=0):
+    self.mat = [[1, 0, x],
+                [0, 1, y],
+                [0, 0, 1]]
+
+# Skaliranje nasledjuje geom. trans.
+class skal(geom):
+  def __init__(self, x=1, y=1):
+    self.mat = [[x, 0, 0],
+                [0, y, 0],
+                [0, 0, 1]]
+
+# Smicanje nasledjuje geom. trans.
+class smic(geom):
+  def __init__(self, x=0, y=0):
+    self.mat = [[1, x, 0],
+                [y, 1, 0],
+                [0, 0, 1]]
+
+# Rotacija nasledjuje geom. trans.
+class rot(geom):
+  def __init__(self, u=0):
+    pom1 = cos(radians(u))
+    pom2 = sin(radians(u))
+    
+    self.mat = [[pom1, -pom2,  0],
+                [pom2,  pom1,  0],
+                [0,      0,    1]]
+
+# Refleksija nasledjuje geom. trans.
+class refl(geom):
+  def __init__(self, u=0):
+    pom1 = cos(radians(u))
+    pom2 = sin(radians(u))
+    
+    self.mat = [[pom1,  pom2,  0],
+                [pom2, -pom1,  0],
+                [0,      0,    1]]
+
+# Tacka predstavljena koordinatama
+class tacka:
+  def __init__(self, x=0, y=0, w=1):
+    if (isinstance(x,list)):
+      self.mat = x
+    else:
+      self.mat = [x, y, w]
+  
+  # Uobicajena string predstava tacke
+  def __str__(self):
+    return str(self.mat)
+
+# Fja za testiranje implementiranih klasa;
+# predlaze se pokretanje sa < test.txt
+def test():
+  print('Unosite linije koda do kraja ulaza.\n'
+        'Podrzano: dodela, kompozicija, stampanje.\n'
+        'Transformacije: translacija, skaliranje,\n'
+        'smicanje, rotacija, refleksija.')
+  
+  # Mali interpretator koji se oslanja
+  # na pozivanje Pajtonovog interpretera
+  while True:
+    try:
+      linija = input()
+      exec(linija)
+    except EOFError:
+      print('Kraj ulaza.')
+      break
+    except Exception:
+      print('Pokusajte ponovo.')
+
+if __name__ == '__main__':
+  test()
+
--- main.py
+++ main.py
@@ -1,73 +1,12 @@
-# Ukljucivanje sistemskog i grafickog modula
-import sys, tkinter
+#!/usr/bin/env python3
 
-# Ukljucivanje pomocnog modula za kutijice sa porukama
-from tkinter import messagebox
-
-# Nosilac programa je klasa GeoDemonstrator, koja
-# nasledjuje graficku klasu Tk iz modula tkinter
-class GeoDemonstrator(tkinter.Tk):
-  # Konstruktor aplikacije
-  def __init__(self):
-    # Pozivanje konstruktora roditeljske klase
-    super(GeoDemonstrator, self).__init__()
-    
-    # Postavljanje naziva aplikacije
-    self.title('GeoDemonstrator')
-    
-    # Inicijalizacija elemenata GKI
-    self.inicijalizacija()
-  
-  # Inicijalizacija elemenata GKI
-  def inicijalizacija(self):
-    # Postavljanje velicine i pozicije prozora
-    self.geometry('450x450+75+75')
-    
-    # Onemogucavanje promene velicine prozora,
-    # posto je Tk prilicno plastican, pa promene
-    # ugrozavaju zamisljeni izgled aplikacije
-    self.resizable(False, False)
-    
-    # Postavljanje glavnog menija i vezivanje
-    # komandi za odgovarajuce funkcionalnosti
-    meni = tkinter.Menu(self)
-    meni.add_command(label = 'Info (F1)', command = self.info)
-    meni.add_command(label = 'Kraj (Esc)', command = self.kraj)
-    self.config(menu = meni)
-    
-    # Vezivanje tipki za akcije analogne
-    # onima iz prethodno postavljenog menija
-    self.bind('<F1>', self.info)
-    self.bind('<Escape>', self.kraj)
-  
-  # Prikazivanje glavnih informacija o aplikaciji;
-  # *args je neophodan kako bi se prosledili dodatni
-  # podaci o dogadjaju tastature, slicno kao sto fje
-  # dogadjaja u GLUT-u obavezno primaju koordinate
-  def info(self, *args):
-    messagebox.showinfo('Informacije',
-               'GeoDemonstrator, seminarski iz Programskih paradigmi.\n\n'
-               'Zamisao je da korisnik zada mnogougao u dvodimenzionom'
-               ' okruženju, nad kojim zatim vrši proizvoljne afine geometrijske'
-               ' tranformacije: translaciju, rotaciju, refleksiju, skaliranje,'
-               ' smicanje.\n\n'
-               'Ideja je omogućiti jednostavno interaktivno prikazivanje i lakše'
-               ' razumevanje materije koja se obrađuje na časovima Geometrije'
-               ' za I smer, kao i Računarske grafike.\n\n'
-               'Autori (tim KriLa):\n'
-               'Kristina Pantelić, 91/2016,\n'
-               'Lazar Vasović, 99/2016.\n\n'
-               'Matematički fakultet, 2019.')
-  
-  # Zatvaranje aplikacije na zahtev korisnika
-  def kraj(self, *args):
-    print('GeoDemonstrator zatvoren na zahtev korisnika!')
-    self.quit()
+# Ukljucivanje modula za GKI
+import gki
 
 # Glavna (main) fja
 def main():
   # Instanciranje stozerne klase
-  aplikacija = GeoDemonstrator()
+  aplikacija = gki.GeoDemonstrator()
   
   # Pokretanje glavne petlje programa
   aplikacija.mainloop()
