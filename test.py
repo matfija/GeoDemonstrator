ab8b65c05bd5ab937dd5c02ea582f2e471c5de32
matfija <lazar1997@mts.rs>
2019-04-29 21:26:35 +0200

    ...

--- test.py
+++ test.py
@@ -10,8 +10,7 @@ t3 = skal(1/7, 1/7)
 
 # Matrica prelaza sa platna na koord. sistem
 plat_u_koord = t3 * t2 * t1
-print(plat_u_koord)
-print()
+print(plat_u_koord, end = '\n\n')
 
 # Definisanje inverznih transformacija
 t1 = trans(204, 132)
@@ -20,25 +19,20 @@ t3 = skal(7, 7)
 
 # Matrica prelaza sa koord. sistema na platno
 koord_u_plat = t1 * t2 * t3
-print(koord_u_plat)
-print()
+print(koord_u_plat, end = '\n\n')
 
 # Definisanje tačke platna
-tac_plat = tacka(202, 129)
-print(tac_plat)
-print()
+tac_plat = tacka(205, 131)
+print(tac_plat, end = '\n\n')
 
 # Prelazak u koord. sistem
 tac_koord = plat_u_koord * tac_plat
-print(tac_koord)
-print()
+print(tac_koord, end = '\n\n')
 
 # Skaliranje u koord. sistemu
 tac_skal_koord = skal(2,2) * tac_koord
-print(tac_skal_koord)
-print()
+print(tac_skal_koord, end = '\n\n')
 
 # Vraćanje u platno
 tac_skal_plat = koord_u_plat * tac_skal_koord
 print(tac_skal_plat)
-print()
